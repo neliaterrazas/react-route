3d6458bf59d8c05136b5d27251e2b359444b0949
Nayomi Morita and Nelia Terrazas <nayomi+nelia@>
2019-04-17 09:47:32 -0700

    add initial build

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+  // const test = {
+  //   color: 'red'
+  // }
+  return(
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+
+}
+
+export default App;
